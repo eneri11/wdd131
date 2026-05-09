4a0335f7b84acfe8f9350939984d751e1f85fa1d
eneri11 <icostales@byupathway.edu>
2026-05-09 17:24:11 +0800

    commit

---
+++ MyFirstConsoleApp/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("What is your Name?");
+ var name = Console.ReadLine();
+var currentDate = DateTime.Now;
+Console.WriteLine($"{Environment.NewLine} Hello, {name}, on {currentDate:d}");
+Console.Write($"{Environment.NewLine}Press any key to exit...");
+Console.ReadKey(true);
+    
