8ae34a2d25fc9780ba2179494726cee0ba5610e2
eneri11 <icostales@byupathway.edu>
2026-05-14 21:16:08 +0800

    commit

--- week02/scripts/numberofdays.js
+++ week02/scripts/numberofdays.js
@@ -8,11 +8,18 @@ let todaystring = new Intl.DateTimeFormat("en-US", options).format(today);
 document.getElementById("today").innerHTML = `Today is <strong>${todaystring}</strong>`;
 
 // next n days
-for (let i = 1; i <= numberOfDays; i++) {
+let i = 1;
+
+while (i <= numberOfDays) {
 	const nextday = new Date();
 	nextday.setDate(today.getDate() + i);
+
 	let nextdaystring = new Intl.DateTimeFormat("en-US", options).format(nextday);
-	const item = document.createElement("li"); // list item
+
+	const item = document.createElement("li");
 	item.textContent = nextdaystring;
+
 	document.querySelector("ul").appendChild(item);
+
+	i++;
 }
