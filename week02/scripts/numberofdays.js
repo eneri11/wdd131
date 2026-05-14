d33ffb8346a32ead1b4c42eef4d281d4a70130c2
eneri11 <icostales@byupathway.edu>
2026-05-14 21:34:44 +0800

    commit

--- week02/scripts/numberofdays.js
+++ week02/scripts/numberofdays.js
@@ -1,20 +1,26 @@
 const numberOfDays = 6; // variable days in future setting
 const options = { weekday: "long" }; // Intl.DateTimeFormat vs. short, etc.
 
-// BEGIN
-const today = new Date();
-// TODAY test output
-let todaystring = new Intl.DateTimeFormat("en-US", options).format(today);
-document.getElementById("today").innerHTML = `Today is <strong>${todaystring}</strong>`;
 
-// next n days
+const today = new Date();
+
+let todaystring = new Intl.DateTimeFormat("en-US", options).format(today);
+
+document.getElementById("today").innerHTML = 
+`Today is <strong>${todaystring}</strong>`;
+
+
 let i = 1;
 
 while (i <= numberOfDays) {
+
 	const nextday = new Date();
+
 	nextday.setDate(today.getDate() + i);
 
-	let nextdaystring = new Intl.DateTimeFormat("en-US", options).format(nextday);
+	let nextdaystring = 
+    
+    new Intl.DateTimeFormat("en-US", options).format(nextday);
 
 	const item = document.createElement("li");
 	item.textContent = nextdaystring;
