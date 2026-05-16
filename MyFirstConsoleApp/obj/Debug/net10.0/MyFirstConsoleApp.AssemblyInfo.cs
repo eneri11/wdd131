8d3e6fcc91dddc5a82360af220ebe02cdc1534cf
eneri11 <icostales@byupathway.edu>
2026-05-16 18:07:05 +0800

    commit

--- MyFirstConsoleApp/obj/Debug/net10.0/MyFirstConsoleApp.AssemblyInfo.cs
+++ MyFirstConsoleApp/obj/Debug/net10.0/MyFirstConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyFirstConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b851f473cb7ac46b2e9b801a24a1f65afce04952")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d073d2cf8a729ef09aaf060bdeb644f13a5e094")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyFirstConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyFirstConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
