4378f31f0ffbd2f8749f7c65e8d4c5db437973ff
eneri11 <icostales@byupathway.edu>
2026-05-16 18:35:10 +0800

    ommit

---
+++ scripts/temples.js
@@ -0,0 +1,27 @@
+// FOOTER YEAR
+
+const currentYear = document.querySelector("currentYear");
+currentYear.textContent = new Date().getFullYear();
+
+//LAST MODIFIED
+
+const lastModified = document.querySelector("#lastModified");
+lastModified.textContent = `Last Modification: ${document.lastModified}`;
+
+// HAMBURGER MENU
+
+const menuButton = document.querySelector("#menuButton");
+const navMenu = document.querySelector("#navMenu");
+
+navMenu.classList.add("hide");
+
+menuButton.addEventListener("click", () => {
+
+    navMenu.classList.toggle("hide");
+
+    if (navMenu.classList.contains("hide")) {
+        menuButton.textContent = "☰";
+    } else {
+        menuButton.textContent = "✖"
+    }
+});
