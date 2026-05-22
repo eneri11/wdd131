13a0ab93d1d51ddc568d61ed55040c158e23a8f0
eneri11 <icostales@byupathway.edu>
2026-05-22 18:01:10 +0800

    commit

---
+++ scripts/place.js
@@ -0,0 +1,28 @@
+const year = document.querySelector("#currentyear");
+year.textContent = new Date().getFullYear();
+
+
+const lastModified = document.querySelector("lastModified");
+lastModified.textContent = 'Last Modified: ${document.lastModified}';
+
+
+const temperature = 9;
+const windSpeed = 5;
+
+
+function calculateWindChill(temp, speed) {
+    return (
+        13.12 + 
+        0.6215 * temp -
+        11.37 * Math.pow(speed, 0.16) +
+        0.3965 * temp * Math.pow(speed, 0.16)
+    ).toFixed(1);
+}
+
+const windChill = document.querySelector("#windchill");
+
+if (temperature <= 10 && windSpeed > 4.8) {
+    windChill.textContent = '${calculateWindChill(temperature, windSpeed)} °C';
+} else {
+    windChill.textContent = "N/A";
+}
